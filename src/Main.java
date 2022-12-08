788c2acfbdd25ebf7fd066b0b901f042592c38a3
CatUnderGlue <warmonds1996@gmail.com>
2022-12-08 12:39:44 +0500

    Задачи 1-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Домашка
+        // Задача 1
+        // С помощью цикла for выведите в консоль все числа от 1 до 10.
+        System.out.println("Задача №1");
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+
+        // Задача 2
+        // С помощью цикла for выведите в консоль все числа от 10 до 1.
+        System.out.println("Задача №2");
+        for (int i = 10; i >= 1; i--){
+            System.out.println(i);
+        }
+
+        // Задача 3
+        // Выведите в консоль все четные числа от 0 до 17.
+        System.out.println("Задача №3");
+        for (int i = 0; i <= 17; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+
+        // Задача 4
+        // Выведите в консоль все числа от 10 до −10 от бо́льшего числа к меньшему.
+        System.out.println("Задача №4");
+        for (int i = 10; i >= -10; i--){
+            System.out.println(i);
+        }
     }
 }
